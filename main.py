3aebbabfb2b98b37f840f6bd66b53c7adf9d7d05
alorsunique <sounique7800@gmail.com>
2024-07-30 23:15:21 +0800

    2024 July 30

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def create_table(database_name, table_name):
         cursor.execute(sql)
         cursor.close()
         connector.commit()
+        connector.close()
     except sqlite3.Error as error:
         print(error)
 
@@ -42,7 +43,7 @@ def show_all_table(database_name):
 
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
         table_list = cursor.fetchall()
-
+        connector.close()
         return table_list
 
     except sqlite3.Error as error:
@@ -53,14 +54,63 @@ def show_table_content(database_name, table_name):
     cursor = connector.cursor()
 
     cursor.execute(f"SELECT * FROM {table_name}")
-    print(cursor.fetchall())
+
+    table_content = cursor.fetchall()
+    cursor.close()
+    connector.close()
+
+    return table_content
 
 def remove_table(database_name, table_name):
     connector = sqlite3.connect(database_name)
     cursor = connector.cursor()
 
     cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+    cursor.close()
+    connector.commit()
 
+def insert_row(database_name, table_name, content):
+    connector = sqlite3.connect(database_name)
+    cursor = connector.cursor()
+
+    get_length = cursor.execute(f"SELECT COUNT(*) FROM {table_name}").fetchone()[0]
+
+    print(get_length)
+    print(type(get_length))
+
+    if get_length == "0":
+        true_content = (1,content[0],content[1])
+    else:
+        cursor.execute(f"""
+                    SELECT * FROM {table_name}
+                    ORDER BY id DESC
+                    LIMIT 1
+                """)
+
+        # Fetch the last row
+        last_row = cursor.fetchone()
+        print("last row")
+        print(last_row)
+        print("last id")
+        last_id = last_row[0]
+        print(type(last_id))
+
+        true_content = (last_id + 1,content[0],content[1])
+
+    sql = f'''
+        INSERT INTO {table_name} (id, name, number)
+        VALUES(?,?,?)
+    '''
+
+    print(sql)
+
+    cursor.execute(sql,true_content)
+
+
+    cursor.close()
+
+    connector.commit()
+    connector.close()
 
 def main_loop():
 
@@ -89,6 +139,31 @@ def main_loop():
         elif choice == "2":
             table_name = input(f"Input table name: ")
             remove_table(database_name, table_name)
+        elif choice == "3":
+            table_name = input(f"Input table name: ")
+            print(f"Tables: {show_all_table(database_name)}")
+
+            print(f"1. Show Content")
+            print(f"2. Add Row")
+            print(f"3. Remove Row")
+
+            explore_choice = input(f"Input table name: ")
+
+            if explore_choice == "1":
+                table_content = show_table_content(database_name,table_name)
+
+                print(table_content)
+            elif explore_choice == "2":
+
+                input_name = str(input())
+                input_number = int(input())
+
+                input_tuple = (input_name,input_number)
+
+                insert_row(database_name,table_name,input_tuple)
+            else:
+                print("Exit")
+
         else:
             print(f"Did not catch that")
 
